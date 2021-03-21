a734f21fd689d6fae1f37146a6b275448a8e1ec7
Yano <Yano@localhost>
2021-03-21 20:52:30 +0900

    useReducer()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,78 @@
+import { useReducer, useState } from "react";
+
 function App() {
-  return <div>App</div>;
+  const [swid, setSwid] = useState(1);
+
+  let [state, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "SEARCHING": {
+          return { ...state, searching: true, error: null, response: null };
+        }
+        case "RESOLVED": {
+          return {
+            ...state,
+            searching: false,
+            response: action.response,
+            error: null,
+          };
+        }
+        case "ERROR": {
+          return {
+            ...state,
+            searching: false,
+            response: null,
+            error: action.error,
+          };
+        }
+        default:
+          return state;
+      }
+    },
+    {
+      searching: false,
+      response: null,
+      error: null,
+    }
+  );
+
+  function handleSearch() {
+    dispatch({ type: "SEARCHING" });
+    fetch("https://swapi.dev/api/people/" + swid)
+      .then((response) => {
+        if (response.ok === false) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        dispatch({ type: "RESOLVED", response: json });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch({ type: "ERROR", error: error.message });
+      });
+  }
+
+  let { searching, response, error } = state;
+
+  return (
+    <div>
+      <input
+        name="id"
+        type="text"
+        value={swid}
+        onChange={(event) => {
+          setSwid(event.target.value);
+        }}
+      />
+      <button onClick={() => handleSearch()}>Search</button>
+      <br />
+      {error && <div>Error: {error}</div>}
+      {searching && <div>Searching...</div>}
+      {!searching && response && <>{response.name}</>}
+    </div>
+  );
 }
 
 export default App;
